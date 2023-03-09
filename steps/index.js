741ef7f8c9b3bb136f2c8c87edbdfcf559315f56
jparrker <102701917+jparrker@users.noreply.github.com>
2023-03-08 21:58:36 -0500

    fixed solution

--- steps/index.js
+++ steps/index.js
@@ -17,10 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n, row = 0) {
+function steps(n, row = 0, stair = '') {
   if (n === row) {
     return;
   }
+  if(n === stair.length) {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+  if(stair.length <=row) {
+    stair += "#"
+  } else {
+    stair += " "
+  }
+  steps(n, row, stair)
 }
 
 // function steps(n) {
